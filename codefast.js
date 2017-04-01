09a1ed71e6d228fd6d6f69997240554b0b0f59f2
Sacha Bron <sacha.bron@copperflake.com>
2017-04-01 21:18:28 +0200

    1.0 version

--- codefast.js
+++ codefast.js
@@ -1,16 +1,22 @@
 let Keyboard = require('node-keylogger');
+let request = require("request");
  
-let k = new Keyboard('event0'); // 'event0' is the file corresponding to my keyboard in /dev/input/ 
-k.on('keyup', count);
-k.on('keydown', count);
+let k = new Keyboard('event0'); // 'event0' is the file corresponding to my keyboard in /dev/input/
 k.on('keypress', count);
 k.on('error', console.error);
 
 let counter = 0;
 
 setInterval(function () {
-	console.log(counter);
-}, 5000);
+	let power = counter * 12;
+	console.log(power + '%');
+	
+	request("http://192.168.130.223/on?left=" + power, function(error, response, body) {
+		// console.log(body);
+	});
+
+	counter = 0;
+}, 1000);
 
 function count() {
 	counter++;
